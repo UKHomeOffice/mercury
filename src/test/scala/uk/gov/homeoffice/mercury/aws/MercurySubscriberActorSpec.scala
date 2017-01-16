e97d0d202af349f6fea9b709656bc8c27030c7dc
David Ainslie <dainslie@gmail.com>
2017-01-16 18:20:29 +0000

    SQS subscription test

--- src/test/scala/uk/gov/homeoffice/mercury/aws/MercurySubscriberActorSpec.scala
+++ src/test/scala/uk/gov/homeoffice/mercury/aws/MercurySubscriberActorSpec.scala
@@ -1,28 +1,41 @@
 package uk.gov.homeoffice.mercury.aws
 
-import java.util.UUID
 import akka.actor.Props
 import org.specs2.concurrent.ExecutionEnv
 import org.specs2.mutable.Specification
 import uk.gov.homeoffice.akka.{ActorExpectations, ActorSystemSpecification}
 import uk.gov.homeoffice.aws.sqs.subscription.Subscriber
-import uk.gov.homeoffice.aws.sqs.{EmbeddedSQSServer, Queue}
+import uk.gov.homeoffice.aws.sqs.{EmbeddedSQSServer, Publisher, Queue}
 
 class MercurySubscriberActorSpec(implicit env: ExecutionEnv) extends Specification with ActorSystemSpecification {
   trait Context extends ActorSystemContext with ActorExpectations with EmbeddedSQSServer {
-    val queueName = UUID.randomUUID().toString
-    val queue = create(new Queue(queueName))
+    val queue = create(new Queue("test-queue"))
   }
 
   "AWS SQS message" should {
-    "be captured as plain text via subscription" in new Context {
+    "be captured directly as plain text" in new Context {
       val message = "A plain text message"
 
       val subscriberActor = system actorOf Props(new MercurySubscriberActor(new Subscriber(queue)))
       subscriberActor ! createMessage(message)
 
       eventuallyExpectMsg[String] {
-        case response => response == s"Received: $message"
+        case response => response == s"Handled message: $message"
+      }
+    }
+
+    "be captured as plain text via subscription" in new Context {
+      implicit val listeners = Seq(testActor)
+
+      val message = "A plain text message"
+
+      val subscriberActor = system actorOf Props(new MercurySubscriberActor(new Subscriber(queue)))
+
+      val publisher = new Publisher(queue)
+      publisher publish message
+
+      eventuallyExpectMsg[String] {
+        case response => response == s"Handled message: $message"
       }
     }
   }
