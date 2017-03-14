f4469241c2a29edf44d9c5bcba899c25cac2c915
David Ainslie <dainslie@gmail.com>
2017-03-14 17:14:40 +0000

    Clustering

--- src/main/scala/uk/gov/homeoffice/mercury/boot/RoutingsBoot.scala
+++ src/main/scala/uk/gov/homeoffice/mercury/boot/RoutingsBoot.scala
@@ -1,10 +1,11 @@
 package uk.gov.homeoffice.mercury.boot
 
 import uk.gov.homeoffice.akka.http.{AkkaHttpBoot, AkkaHttpConfig}
+import uk.gov.homeoffice.configuration.HasConfig
 import uk.gov.homeoffice.mercury.routing.{HealthCheckRouting, WelcomeRouting}
 
-trait RoutingsBoot extends AkkaHttpBoot {
+trait RoutingsBoot extends AkkaHttpBoot with HasConfig {
   this: App =>
 
-  boot(WelcomeRouting, HealthCheckRouting)(AkkaHttpConfig())
+  boot(WelcomeRouting, HealthCheckRouting)(AkkaHttpConfig(port = config.int("akka.http.port", 9100)))
 }
