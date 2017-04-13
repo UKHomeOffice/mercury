f9cf26618813b9838f5e79e68a2f68d4c4c4afef
Tamer Moustafa <tamer.moustafa@transformuk.com>
2017-04-13 14:33:39 +0100

    Handle invalid case type error from Hocs

--- src/main/scala/uk/gov/homeoffice/mercury/Mercury.scala
+++ src/main/scala/uk/gov/homeoffice/mercury/Mercury.scala
@@ -39,7 +39,7 @@ object Mercury {
           override val token: String = (response.json \ "data" \ "ticket").as[String]
         }
 
-        case _ => Future failed new Exception(s"""Failed to authorize against "${webService.host}" because of: Http response status ${response.status}, ${response.statusText}""")
+        case _ => Future failed new Exception(s"""Failed to authorize against "${webService.host}"$authorizationEndpoint because of: Http response status ${response.status}, ${response.statusText}""")
       }
     }
   }
@@ -87,10 +87,12 @@ class Mercury(val s3: S3, val webService: WebService with Authorization) extends
     val data = StreamConverters.fromInputStream(() => new FileInputStream(pdf))
     val filePart = FilePart("file", pdf.getName, Some(`application/pdf`), data)
 
-    info(s"""Publishing to endpoint ${webService.host}$publicationEndpoint, resource with S3 key "${resource.key}"""")
+    val caseType = email.to.substring(0, email.to.indexOf("@")).toUpperCase
+
+    info(s"""Publishing Case type $caseType to endpoint ${webService.host}$publicationEndpoint, resource with S3 key "${resource.key}"""")
 
     webService endpoint publicationEndpoint withQueryString authorizationParam post Source(List(
-      DataPart("caseType", email.to.substring(0, email.to.indexOf("@"))), filePart)
+      DataPart("caseType", caseType), filePart)
     ) map { response =>
       response.status match {
         case OK =>
@@ -104,8 +106,18 @@ class Mercury(val s3: S3, val webService: WebService with Authorization) extends
 
           Publication(Try(response.json).map(jValue).getOrElse(JNothing))
 
+        case INTERNAL_SERVER_ERROR =>
+          //If the case type (i.e. the email recipient) is invalid then just log it
+          if (response.body.contains(s"The value is not an allowed value: $caseType")) {
+            error(s"""Failed to publish to "${webService.host}$publicationEndpoint" because the Case type $caseType is invalid""")
+            s3.s3Client.deleteObject(s3.bucket, resource.key)
+            Publication(JNothing)
+          } else {
+            throw new Exception(s"""Failed to publish to "${webService.host}$publicationEndpoint" because of: Http response status ${response.status}, ${response.statusText} with body:\n${response.body}""")
+          }
+
         case _ =>
-          throw new Exception(s"""Failed to publish to "${webService.host}" because of: Http response status ${response.status}, ${response.statusText} with body:\n${response.body}""")
+          throw new Exception(s"""Failed to publish to "${webService.host}$publicationEndpoint" because of: Http response status ${response.status}, ${response.statusText} with body:\n${response.body}""")
       }
     } andThen { case _ =>
       pdf.delete()
@@ -118,12 +130,12 @@ class Mercury(val s3: S3, val webService: WebService with Authorization) extends
 
     webService endpoint attachmentEndpoint withQueryString authorizationParam post Source(List(
       DataPart("name", emailAttachment.name), DataPart("destination", caseRef),
-      DataPart("documenttype", emailAttachment.contentType), DataPart("documentdescription", "Email attachment"), filePart)
+      DataPart("documenttype", "Original"), DataPart("documentdescription", "Email attachment"), filePart)
     ) map { response =>
       response.status match {
         case OK => {}
         case _ =>
-          error(s"""Failed to publish attachment to "${webService.host}" because of: Http response status ${response.status}, ${response.statusText} with body:\n${response.body}""")
+          error(s"""Failed to publish attachment to "${webService.host}$attachmentEndpoint" because of: Http response status ${response.status}, ${response.statusText} with body:\n${response.body}""")
       }
     }
   }
